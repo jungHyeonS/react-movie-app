4bd549daf0dd819a01426d1f23e854665c1be7a7
jungHyeonS <fbtls25@naver.com>
2022-08-06 13:39:10 +0900

    react-movie-app : To do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [toDo,setToDo] = useState("")
+  const [toDos,setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) =>{
+    event.preventDefault()
+    if(toDo === ""){
+      return;
+    }
+
+    // ... 으로 현재 배열을 가져와서 새로운 값과 합쳐서 새로운 배열을 만든다
+    setToDos(currentArray => [toDo,...currentArray])
+    setToDo("")
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do.."/>
+        <button>Add To Do</button>
+      </form>
+      <hr></hr>
+
+      {/* map : 하나의 배열에 있는 item을 내가 원하는 무엇이든지로 바꿔주는 역할 */}
+      <ul>
+        {toDos.map((item,index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      
+      
     </div>
   );
 }
