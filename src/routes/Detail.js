ea17e9600b9473cf218b9b50df556aaa91fe5d3a
jungHyeonS <fbtls25@naver.com>
2022-08-07 16:51:17 +0900

    movie app : detail app수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,29 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import {useParams} from "react-router-dom"
 function Detail(){
     const {id} = useParams()
-
+    const [loading,setLoading] = useState(true)
+    const [detail, setDetail] = useState({})
     const getDetail = async() => {
         const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json()
+        setDetail(json.data.movie)
+        setLoading(false)
         console.log(json)
     }
 
     useEffect(()=>{
         getDetail()
     },[])
-    return <h1>Detail</h1>
+    return (
+        <div>
+            <h1>Detail</h1>
+            {loading ? <h2>Loading</h2> : (
+                <div>
+                    {detail.title}<br></br>
+                    <img src={detail.medium_cover_image}/>
+                </div>
+            )}
+        </div>
+    );
 }
 export default Detail
